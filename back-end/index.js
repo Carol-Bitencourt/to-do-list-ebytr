d8f77ea2fa7aadc4e2a4c010b437621cd5a6df88
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-11-03 19:10:26 -0300

    Rotas, rescue e eslint

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,18 @@
-// index back-end
+const express = require('express');
+
+const app = express();
+const todoRouter = require('./routers/todoRouter');
+
+app.use(express.json());
+app.use('/todo', todoRouter);
+
+app.use((err, _req, res, _next) => {
+  console.log('ERRRÃO', err);
+  return res.status(err.status).json(err.error);
+});
+
+app.get('/', (_req, res) => {
+  res.send();
+});
+
+app.listen(3001, () => console.log('ouvindo porta 3001!'));
